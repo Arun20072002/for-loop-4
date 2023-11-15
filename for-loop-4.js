652f2e3d50d7a3bb582ab460bdaf5a2851213ccf
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-15 09:56:37 +0530

    for-loop-4

---
+++ for-loop-4.js
@@ -0,0 +1,14 @@
+document.write("print odd number 1 to 100"+"<br>")
+document.write("-------------------------"+"<br>")
+
+for( let a=1;a<=100;a++){
+    if(a%2!==0){
+        document.write(a+"<br>")
+    }
+}
+
+for(let b=1;b<=100;b++){
+    if(b/3){
+
+    }
+}
